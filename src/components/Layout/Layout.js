0aee4f80eae21561fa56fa0b44ab8092d942b155
Selena Pham <selena40253@gmail.com>
2025-03-04 13:21:56 -0800

    Added responsive styling.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import './Layout.css';
 
 const Layout = () => {
+    const [openBars, setOpenBars] = useState(false);
+
+    const openMenu = () => {
+        if (openBars) {
+            document.querySelector(".verticalMenu").style.display = "none";
+            setOpenBars(false);
+        } else {
+            document.querySelector(".verticalMenu").style.display = "block";
+            setOpenBars(true);
+        }
+    }
+
     return (
         <>
             <nav>
-                <ul>
+                <button className="barMenuButton" onClick={openMenu}>&#9776;</button>
+                <ul className="horizontalMenu">
+                    <li><Link to="/">To Do</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+                <ul className="verticalMenu">
                     <li><Link to="/">To Do</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                 </ul>
